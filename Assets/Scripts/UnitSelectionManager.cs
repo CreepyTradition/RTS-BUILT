6ddcb71949102bde68fdb719b96234e1ecf7a4d3
Perkin Abrantes <perkinabrantes@gmail.com>
2025-07-02 18:12:38 +0800

    Initial commit

---
+++ Assets/Scripts/UnitSelectionManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectionManager : MonoBehaviour
+{
+    public static UnitSelectionManager Instance { get; set; }
+
+    public List<GameObject> allUnitsList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+    public LayerMask clickable;
+    public LayerMask ground;
+    public GameObject groundMarker;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+            {
+                
+            }
+        }
+    }
+}
